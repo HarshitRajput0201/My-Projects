6cf693dbda2fb692fba3b7585fab8538e3a65e3b
Harshit <harshitrajput0201@gmail.com>
2024-01-25 11:54:24 +0530

    adding Images

--- src/data.js
+++ src/data.js
@@ -4,13 +4,13 @@ const data = [
     {
         id: 1,
         name: "My Portfolio",
-        image: "/images/Portfolio.jpg",
+        image: "https://res.cloudinary.com/deapwdwgc/image/upload/v1706163757/My%20Projects/Portfolio_fejiry.png",
         info: "Welcome to My Portfolio a showcase of my skills, achievements, and creativity. This project is a culmination of my proficiency in HTML, CSS, and JavaScript, designed to present a comprehensive overview of my professional journey and personal projects."
     },
 
     {
         id: 2,
-        name: "Profile-Page",
+        name: "https://res.cloudinary.com/deapwdwgc/image/upload/v1706163756/My%20Projects/Profile_gfezah.jpg",
         image: "/images/Profile.jpg",
         info: "My Profile Page is a personalized and visually appealing web project crafted with HTML, CSS, and JavaScript to showcase and highlight individual identity and skills in a digital space. This project serves as an online representation of your personal and professional journey, providing visitors with a comprehensive glimpse into your personality, expertise, and achievements."
     },
@@ -18,28 +18,28 @@ const data = [
     {
         id: 3,
         name: "Weather App",
-        image: "/images/Weather.jpg",
+        image: "https://res.cloudinary.com/deapwdwgc/image/upload/v1706163756/My%20Projects/Weather_xdmsx2.jpg",
         info: "The Weather App is a dynamic and user-friendly web application created using a combination of HTML, CSS, and JavaScript. This project aims to provide users with real-time weather information in a visually appealing and intuitive manner."
     },
 
     {
         id: 4,
         name: "E-Com Product",
-        image: "/images/E-Com.jpg",
+        image: "https://res.cloudinary.com/deapwdwgc/image/upload/v1706163756/My%20Projects/E-Com_asa5wy.jpg",
         info: "Welcome to the Ecom Product Page, a dynamic and visually appealing web project crafted with the perfect blend of HTML, CSS, and JavaScript. This project is designed to provide users with an immersive and seamless shopping experience, showcasing the features and details of various products in an intuitive and user-friendly manner."
     },
 
     {
         id: 5,
         name: "Calculator",
-        image: "/images/Calculator.jpg",
+        image: "https://res.cloudinary.com/deapwdwgc/image/upload/v1706163756/My%20Projects/Calculator_u9plrn.jpg",
         info: "Welcome to our Interactive Calculator, a versatile tool designed to simplify your mathematical tasks with a user-friendly interface. This project combines the power of HTML, CSS, and JavaScript to create a seamless and efficient calculator experience."
     },
 
     {
         id: 6,
         name: "Blog Website",
-        image: "/images/Blog.jpg",
+        image: "https://res.cloudinary.com/deapwdwgc/image/upload/v1706163756/My%20Projects/Blog_uyyky2.jpg",
         info: "DynamicBlog is a sleek and dynamic blog website that seamlessly combines the power of HTML, CSS, and JavaScript to provide users with a visually appealing and interactive blogging experience. This project represents a culmination of creativity, design, and functionality to deliver a user-friendly platform for sharing thoughts, stories, and ideas."
     }
 
